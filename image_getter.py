8294e78e58c5314ca9f9ba889a6d0580652ce374
Pavloffff <pavlov.id.2003@gmail.com>
2022-10-15 13:56:34 +0300

    change getter

--- image_getter.py
+++ image_getter.py
@@ -5,7 +5,7 @@ import matplotlib.image as mpimg
 import cv2
 import subprocess
 
-class ImageGetter(object):
+class ImageGetter():
     
     def __init__(self, image_name):
         self.inp_path = r'./mmpose/tests/data/aflw/'
@@ -14,12 +14,8 @@ class ImageGetter(object):
         self.outp_name = self.inp_name.split('.')[0] + '.npy'
         self.script = './mmpose/keypoints_script.sh'
         
-    def getitem(self):
+    def __get__(self, index):
         subprocess.call([self.script, self.inp_name])
         self.keypoints = np.load(self.outp_path + self.outp_name)
         self.image = cv2.imread(self.inp_path + self.inp_name)
         return {'image': self.image, 'keypoints': self.keypoints}
-
-    def show(self):
-        plt.imshow(self.image)
-        plt.scatter(self.keypoints[:, 0], self.keypoints[:, 1], s=10, c='r')
